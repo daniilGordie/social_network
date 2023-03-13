cf79e9ae70dee254bc32ed5f512126637df26c61
Daniil <megabaptist17@gmail.com>
2023-03-13 11:21:44 +0200

    users formik added, prepare for useHistory change

--- src/components/UsersPage/Users.tsx
+++ src/components/UsersPage/Users.tsx
@@ -1,34 +1,105 @@
-import React from 'react'
-import { UserType } from '../../types/types'
-import Paginator from '../Paginator/Paginator.tsx'
+import React, { FC, useEffect } from 'react'
+import Paginator from './../Paginator/Paginator.tsx'
 import User from './User/User.tsx'
+import { UsersSearchForm } from './UsersSearchForm.tsx'
+import { FilterType, requestUsers } from '../../redux/users-reducer.ts'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  getCurrentPage,
+  getFollowingInProgress,
+  getPageSize,
+  getTotalUsersCount,
+  getUsers,
+  getUsersFilter,
+} from '../../redux/users-selector.ts'
+import { useHistory } from 'react-router-dom'
+const querystring = require('querystring-es3')
+type PropsType = {}
 
-type PropsType = {
-  usersList: Array<UserType>
-  currentPage: number
-  followThunk: () => void
-  followingInProgress: Array<number>
-  onPageChanged: (pageNumber: number) => void
-  pageSize: number
-  totalItemsCount: number
-  unfollowThunk: () => void
-}
+type QueryParamsType = { term?: string; page?: string; friend?: string }
+export const Users: FC<PropsType> = (props) => {
+  const users = useSelector(getUsers)
+  const totalUsersCount = useSelector(getTotalUsersCount)
+  const currentPage = useSelector(getCurrentPage)
+  const pageSize = useSelector(getPageSize)
+  const filter = useSelector(getUsersFilter)
+  const followingInProgress = useSelector(getFollowingInProgress)
 
-const Users: React.FC<PropsType> = ({ usersList, ...props }) => {
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  useEffect(() => {
+    const parsed = querystring.parse(history.location.search.substr(1)) as QueryParamsType
+
+    let actualPage = currentPage
+    let actualFilter = filter
+
+    if (!!parsed.page) actualPage = Number(parsed.page)
+
+    if (!!parsed.term) actualFilter = { ...actualFilter, term: parsed.term as string }
+
+    switch (parsed.friend) {
+      case 'null':
+        actualFilter = { ...actualFilter, friend: null }
+        break
+      case 'true':
+        actualFilter = { ...actualFilter, friend: true }
+        break
+      case 'false':
+        actualFilter = { ...actualFilter, friend: false }
+        break
+    }
+
+    dispatch(requestUsers(actualPage, pageSize, actualFilter))
+  }, [])
+
+  useEffect(() => {
+    const query: QueryParamsType = {}
+
+    if (!!filter.term) query.term = filter.term
+    if (filter.friend !== null) query.friend = String(filter.friend)
+    if (currentPage !== 1) query.page = String(currentPage)
+
+    history.push({
+      pathname: '/developers',
+      search: querystring.stringify(query),
+    })
+  }, [filter, currentPage])
+
+  const onPageChanged = (pageNumber: number) => {
+    dispatch(requestUsers(pageNumber, pageSize, filter))
+  }
+  const onFilterChanged = (filter: FilterType) => {
+    dispatch(requestUsers(1, pageSize, filter))
+  }
+  const follow = (userId: number) => {
+    dispatch(follow(userId))
+  }
+  const unfollow = (userId: number) => {
+    dispatch(unfollow(userId))
+  }
+
   return (
-    <>
-      <Paginator {...props} />
-      {Array.from(usersList).map((u) => (
-        <User
-          key={u.id}
-          user={u}
-          followingInProgress={props.followingInProgress}
-          unfollowThunk={props.unfollowThunk}
-          followThunk={props.followThunk}
-        />
-      ))}
-    </>
+    <div>
+      <UsersSearchForm onFilterChanged={onFilterChanged} />
+
+      <Paginator
+        currentPage={currentPage}
+        onPageChanged={onPageChanged}
+        totalItemsCount={totalUsersCount}
+        pageSize={pageSize}
+      />
+      <div>
+        {users.map((u) => (
+          <User
+            user={u}
+            followingInProgress={followingInProgress}
+            key={u.id}
+            unfollow={unfollow}
+            follow={follow}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
-
-export default Users
